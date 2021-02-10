cbd51dedd798944f8aa4553c075d556e5df72d39
Jacob A <jacobatzori@hotmail.com>
2021-02-10 17:55:12 -0500

    simple loop and axios call

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello World");
+let axios = require("axios");
+
+let minutes = 0.1;
+let interval = minutes * 60 * 1000;
+
+loop = (todo, interval) => {
+  setInterval(() => {
+    console.log("executing");
+    todo();
+  }, interval);
+}
+
+run = () => {
+  axios.post("google.ca", {})
+  .then((res) => {
+    console.log(res.response);
+  })
+  .catch((err) => {
+    console.log(`${err.code} : ${err.response}`);
+  });
+}
+
+// loop(run, interval);
+
+run();
